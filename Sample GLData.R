2622735cdbfae864da2d3ab81e9754370e903042
Zucchet <Chris.Zucchet@FTIConsulting.com>
2021-07-14 14:42:23 +0100

    Changes to GL and P2P data

--- Sample GLData.R
+++ Sample GLData.R
@@ -1,21 +1,33 @@
 ##########     1) Load required packages/data         ###############
 library(purrr);library(tidyverse);library(magrittr);library(randomNames);library(OpenRepGrid);library(readxl);library(caroline);library(stringr)
+setwd("C:/Users/czucche/Documents/R/AMLSandpit")
 coa = read_excel("Dummy CoA.xlsx") %>% data.frame()
 
+
+setwd("C:/Users/czucche/Desktop/Use Case Development/Augmented Restructuring/2) Data")
+
 ##########     2) Define parameters         ###############
-n_lines = 150000;set.seed(100)
+n_lines = 2500000;set.seed(100)
 n_users = 57
 n_prods = 15
 n_vendors = 27
-n_customers = 863
-start_date = '2020/01/01'; end_date = '2021/12/31'
+n_customers = 12
+start_date = '2019/01/01'; end_date = '2021/12/31'
 
 
 ##########     2) Define input fields data    ###############
 comp_codes      =seq(1000,1020,5)
+profit_cntr_codes  =seq(1,657,1);
 cost_cntr_codes  =seq(25000,26000,100);
 rpt_days_diff = c(0,1,2,3,-10,-30)
-users = seq(1,n_users,1); products =  seq(1,n_prods,1); vendors = seq(1,n_vendors,1); customers = seq(1,n_customers,1)
+users = seq(1,n_users,1); products =  seq(1,n_prods,1); vendors = seq(1,n_vendors,1); 
+
+customers = c("Family first high spenders", "Just for me working young professionals","Empty nest elderlies","Environmentally conscious entreprenuers","Asset rich adults","Budget focused baby boomers",  "Meme focused millenials", "Mission enabled metropolitans","Respendent retirees","Rural retirees", "Income heavy independents","Capital endowed cosmopolitans")
+customers = data.frame(CUST_ID = 1:12,CUST_DESC = customers )
+write.delim(customers, "CUSTOMER_DETAIL.txt" ,sep = "|")
+
+
+customers = seq(1,n_customers,1)
 doc_types = c("Bank Statement","Cash Disbursement","Cash Receipt","Cash Transfer","Check Deposit","Clearing","Customer Credit Memo","Customer Invoice","Expense Assignment to Sales Documents","External Service Confirmation","Goods Receipt/Invoice Receipt Clearing","Internal Service Confirmation","Journal Entry Voucher","Other Charge/Credit","Revenue Recognition","Settlement","Supplier Credit Memo","Supplier Invoice")
 jnl_lines = c(2,3,4,5,6,7,8,9,10)
 rpt_curr = c("GBP")
@@ -27,7 +39,7 @@ gl_codes = coa$GL_CODE
 
 ##########     3) Define probabilities for fields    ###############
 comp_probs = c( 0.5, 0.35, 0.2,0.1,0.05)
-cost_cntr_probs = runif(length(cost_cntr_codes))
+cost_cntr_probs = runif(length(cost_cntr_codes));profit_cntr_code_probs = runif(length(profit_cntr_codes))
 rpt_probs = c( 2, 0.5, 0.25,0.1,0.05,0.025)
 user_probs = c(runif(length(users)-2),0.0015,0.0015)  ; products_probs = runif(length(products)); vendors_probs = runif(length(vendors)); cust_probs = runif(length(customers))
 doc_types_probs = runif(length(doc_types))
@@ -43,24 +55,27 @@ jnl_df_t = data.frame(jnl_lines_t, agg_sum = cumsum(jnl_lines_t), line_index = 1
 n_jnls = nrow(jnl_df_t)
 v_desc_length = 2
 
-DOC_TYPE = sample(doc_types, n_jnls,doc_types_probs, replace = T) 
-POST_DATE = sample(seq(as.Date(start_date), as.Date(end_date), by="day"), n_jnls, replace = T) 
+DOC_TYPE = sample(doc_types, n_jnls,doc_types_probs, replace = T)
+POST_DATE = sample(seq(as.Date(start_date), as.Date(end_date), by="day"), n_jnls, replace = T)
 RPT_DATE  = POST_DATE + sample(rpt_days_diff, n_jnls,rpt_probs, replace = T)
-RPT_CURR = sample(rpt_curr, n_jnls,rpt_curr_probs, replace = T) 
+RPT_CURR = sample(rpt_curr, n_jnls,rpt_curr_probs, replace = T)
 LCL_CURR = sample(lcl_curr, n_jnls,lcl_curr_probs, replace = T) 
 COMP_CODE = sample(comp_codes, n_jnls,comp_probs, replace = T)
-COST_CNTR = sample(cost_cntr_codes, n_jnls,cost_cntr_probs, replace = T) 
+PROFIT_CTR = sample(profit_cntr_codes, n_jnls,profit_cntr_code_probs, replace = T)
+COST_CNTR = sample(cost_cntr_codes, n_jnls,cost_cntr_probs, replace = T)
 USER_ID   = sample(users, n_jnls,user_probs, replace = T) 
 JNL_AMT   = sample(jnl_amts, n_jnls,jnl_amt_probs, replace = T) 
 MAN_FLAG  = ifelse(runif(n_jnls) > .68,1,0)
-JNL_DESC  = randomSentences(n_jnls, v_desc_length)
-
+JNL_DESC  = rep(" ", n_jnls)  # randomSentences(n_jnls, v_desc_length)
+ADD_FIELD_1 = rep(" ", n_jnls) 
+ADD_FIELD_2 = rep(" ", n_jnls) 
 
 ##########     5) Create data structure for journals   ###############
 
 journal_id_df = data.frame(jnl_id = as.character(seq(1000000001,(1000000000+nrow(jnl_df_t)),1)),
                            RPT_CURR_T = RPT_CURR, LCL_CURR_T = LCL_CURR,DOC_TYPE_T = DOC_TYPE,POST_DATE_T = POST_DATE,RPT_DATE_T =RPT_DATE,
-                           COMP_CODE_T = COMP_CODE,COST_CNTR_T = COST_CNTR,USER_ID_T = USER_ID, MAN_FLAG_T = MAN_FLAG,JNL_AMT_T = JNL_AMT, JNL_DESC_T = JNL_DESC
+                           COMP_CODE_T = COMP_CODE,PROFIT_CTR_T =PROFIT_CTR, COST_CNTR_T = COST_CNTR,USER_ID_T = USER_ID, MAN_FLAG_T = MAN_FLAG,JNL_AMT_T = JNL_AMT, JNL_DESC_T = JNL_DESC,
+                           ADD_FIELD_1_T = ADD_FIELD_1, ADD_FIELD_2_T = ADD_FIELD_2
                            )
 journal_id = data.frame(jnl_id = as.character(seq(1000000001,(1000000000+nrow(jnl_df_t)),1)))%>% split(.$jnl_id)
 
@@ -68,10 +83,13 @@ journal_id = data.frame(jnl_id = as.character(seq(1000000001,(1000000000+nrow(jn
 for(i in 1:nrow(journal_id_df)){ 
   journal_id[[i]] = data.frame(JNL_ID = rep(journal_id_df$jnl_id[i], jnl_df_t$jnl_lines_t[i]),
                                LINE_ID = 1:jnl_df_t$jnl_lines_t[i],
+                               
+                               
                                RPT_CURR  = rep(journal_id_df$RPT_CURR_T[i], jnl_df_t$jnl_lines_t[i]),LCL_CURR  = rep(journal_id_df$LCL_CURR_T[i], jnl_df_t$jnl_lines_t[i]),
                                DOC_TYPE  = rep(journal_id_df$DOC_TYPE_T[i], jnl_df_t$jnl_lines_t[i]),POST_DATE  = rep(journal_id_df$POST_DATE_T[i], jnl_df_t$jnl_lines_t[i]),
                                RPT_DATE  = rep(journal_id_df$RPT_DATE_T[i], jnl_df_t$jnl_lines_t[i]),COMP_CODE  = rep(journal_id_df$COMP_CODE_T[i], jnl_df_t$jnl_lines_t[i]),JNL_DESC  = rep(journal_id_df$JNL_DESC_T[i], jnl_df_t$jnl_lines_t[i]),
                                COST_CNTR  = rep(journal_id_df$COST_CNTR_T[i], jnl_df_t$jnl_lines_t[i]),USER_ID  = rep(journal_id_df$USER_ID_T[i], jnl_df_t$jnl_lines_t[i]) ,
+                               PROFIT_CTR  = rep(journal_id_df$PROFIT_CTR_T[i], jnl_df_t$jnl_lines_t[i]),ADD_FIELD_1  = rep(journal_id_df$ADD_FIELD_1_T[i], jnl_df_t$jnl_lines_t[i]),ADD_FIELD_2  = rep(journal_id_df$ADD_FIELD_2_T[i], jnl_df_t$jnl_lines_t[i]),
                                MAN_FLAG = rep(journal_id_df$MAN_FLAG_T[i], jnl_df_t$jnl_lines_t[i]),   N_LINES = jnl_df_t$jnl_lines_t[i], JNL_AMT = rep(journal_id_df$JNL_AMT_T[i], jnl_df_t$jnl_lines_t[i]),
                                DBT_LINES = sample(jnl_df_t$jnl_lines_t[i],1), DBT_SIDE = round(journal_id_df$JNL_AMT[i]/2,2)
                                                               )
@@ -91,21 +109,24 @@ for(i in 1:length(gl_temp)){
 ##########     6) Add lines data into journals and create ###############
 ##########      other fields for GL detail                ###############
 
-GL_DETAIL =  bind_rows(gl_temp) %>%   select(COMP_CODE, COST_CNTR, JNL_ID, LINE_ID,DOC_TYPE, POST_DATE, RPT_DATE,RPT_CURR,LCL_CURR, RPT_DBT,RPT_CDT,JNL_DESC,  USER_ID, MAN_FLAG) %>% 
-              mutate(LINE_DESC = randomSentences(n(), v_desc_length), LCL_DBT = ifelse(LCL_CURR == "EUR", RPT_DBT * v_GBP_EUR, ifelse(LCL_CURR == "USD", RPT_DBT * v_GBP_EUR,RPT_DBT) ),
-                     LCL_CDT = ifelse(LCL_CURR == "EUR", RPT_CDT * v_GBP_EUR, ifelse(LCL_CURR == "USD", RPT_CDT * v_GBP_EUR, RPT_CDT) ),
+GL_DETAIL =  bind_rows(gl_temp) %>%   select(COMP_CODE, COST_CNTR,PROFIT_CTR,  JNL_ID, LINE_ID,DOC_TYPE, POST_DATE, RPT_DATE,RPT_CURR,LCL_CURR, RPT_DBT,RPT_CDT,JNL_DESC,  USER_ID, MAN_FLAG,ADD_FIELD_1,ADD_FIELD_2) %>% 
+              mutate(LCL_DBT = ifelse(LCL_CURR == "EUR", RPT_DBT * v_GBP_EUR, ifelse(LCL_CURR == "USD", RPT_DBT * v_GBP_EUR,RPT_DBT) ),
+                     LCL_CDT = ifelse(LCL_CURR == "EUR", RPT_CDT * v_GBP_EUR, ifelse(LCL_CURR == "USD", RPT_CDT * v_GBP_EUR, RPT_CDT) ),LINE_DESC = rep(" ",n()), #randomSentences(n(), v_desc_length),  
                      CUST_ID   = sample(customers, n(),cust_probs, replace = T), VEND_ID   = sample(vendors, n(),vendors_probs, replace = T), PRODUCT_ID   = sample(products, n(),products_probs, replace = T),
-                     GL_CODE = ifelse(RPT_DBT > 0, sample(gl_codes, n(), gl_dbt_probs, replace = T),sample(gl_codes,n(), gl_cdt_probs, replace = T))
-                     )
+                     GL_CODE = ifelse(RPT_DBT > 0, sample(gl_codes, n(), gl_dbt_probs, replace = T),sample(gl_codes,n(), gl_cdt_probs, replace = T))) %>% 
+              mutate(GL_CODE = format(GL_CODE, scientific = FALSE)) %>%
+              filter(as.numeric(GL_CODE) >= 400000)  ### use logic if doing restructuring demo data
 
 ##########     7) Export data for dashboard  ###############
 
 write.delim(GL_DETAIL, "GL_DETAIL.txt", sep = "|")
 
-
 ##########     8) Create tests for exceptions ###############
 options(digits=15)
 JNL_SUMM = GL_DETAIL %>%  group_by(JNL_ID, RPT_DATE, POST_DATE,USER_ID) %>% 
+  summarise(JNL_AMT = round(sum(RPT_DBT),2)) %>% data.frame()
+
+JNL_CONSOL = GL_DETAIL %>%  group_by(JNL_ID) %>% 
   summarise(JNL_AMT = round(sum(RPT_DBT),2)) %>% data.frame()
 
 ##########     9) Test Layer ###############
@@ -132,14 +153,15 @@ T_04_MRJs =  GL_DETAIL %>% filter(MAN_FLAG == 1 & str_sub(GL_CODE,1,1) == "4") %
 
 #### Consolidate tests ####
 
-TEST_RESULTS =  left_join(JNL_SUMM, T_01_RS, by = "JNL_ID") %>% mutate(T_01_IS_RS = ifelse(is.na(T_01_IS_RS) ,0,1)) %>% 
+TEST_RESULTS =  left_join(JNL_CONSOL, T_01_RS, by = "JNL_ID") %>% mutate(T_01_IS_RS = ifelse(is.na(T_01_IS_RS) ,0,1)) %>% 
                   left_join(T_02_BDPs) %>% mutate(T_02_IS_BDPs = ifelse(is.na(T_02_IS_BDPs) ,0,1)) %>% 
                   left_join(T_03_LPUs) %>% mutate(T_03_IS_LPUs = ifelse(is.na(T_03_IS_LPUs) ,0,1)) %>%
                   left_join(T_04_MRJs) %>% mutate(T_04_IS_MREV = ifelse(is.na(T_04_IS_MREV) ,0,1)) %>% 
                   filter(T_01_IS_RS != 0 | T_02_IS_BDPs != 0 | T_03_IS_LPUs != 0 | T_04_IS_MREV != 0) %>% 
-                  select(JNL_ID, T_01_IS_RS, T_02_IS_BDPs, T_03_IS_LPUs, T_04_IS_MREV)
+                  select(JNL_ID,T_01_IS_RS, T_02_IS_BDPs, T_03_IS_LPUs, T_04_IS_MREV)
 
 write.delim(TEST_RESULTS, "TEST_RESULTS.txt", sep = "|")
+
 
 ##########     10) Completeness tests ###############
 
@@ -152,6 +174,37 @@ UNBAL_JNLS = GL_DETAIL %>%  group_by(JNL_ID) %>%
 write.delim(UNBAL_JNLS, "UNBAL_JNLS.txt", sep = "|")
 
 
+
+
+##########     11) Store data ###############
+### Get UK Postcodes - txt file already produced, no need to run twice ###
+UKPostCodes = tbl(bq_conn, "PostCodes") %>% filter(In_Use_ == TRUE & Latitude != 0 & County != "") %>% 
+  select(Postcode, Latitude, Longitude, County, District, Ward, Country, Constituency, Rural_urban, Region, Local_authority,Index_of_Multiple_Deprivation  ) %>%  data.frame() 
+write.delim(UKPostCodes, "UKPostCodes.txt", sep = "|" )
+
+
+
+
+### Read UK Postcodes ###
+UKPostCodes = read_delim("UKPostCodes.txt", delim = "|") %>% data.frame()
+
+set.seed(1234)
+n_stores = length(profit_cntr_codes)
+square_ft = c(seq(6000, 13500,5),seq(6000, 8500,1));square_ft_probs = runif(length(square_ft))
+st_srt_t1 ='2000/01/01';st_srt_t2 ='2010/01/01';st_end_dt = '2021/12/31'; ls_ear_dt = '2022/06/30'  ; ls_end_dt = '2026/12/31'
+lease_duration = seq(as.Date(ls_ear_dt), as.Date(ls_end_dt), by = "month");lease_duration_probs = runif(length(lease_duration))
+desc = c("High Street", "Shopping Center","Freehold shop"); desc_probs = c(0.3,0.25,0.35)
+
+STORE_ID =  profit_cntr_codes
+AREA = UKPostCodes[sample(1:nrow(UKPostCodes),length(STORE_ID),replace = T ),]
+OPEN_DATE = sample(c(seq(as.Date(st_srt_t1), as.Date(st_end_dt), 5),seq(as.Date(st_srt_t2), as.Date(st_end_dt), 2)), n_stores, replace = T)
+LEASE_END_DATE = sample(lease_duration, n_stores, replace = T)
+SQ_FT = sample(square_ft, n_stores, square_ft_probs, replace = T)
+STORE_TYPE = sample(desc, n_stores, desc_probs, replace = T)
+
+STORE_DETAIL = data.frame( STORE_ID= STORE_ID,STORE_TYPE = STORE_TYPE, AREA, OPEN_DATE = OPEN_DATE,LEASE_END_DATE = LEASE_END_DATE, SQ_FT = SQ_FT, REGION_COUNTRY = paste0(AREA$County, ", ", AREA$Country ))
+
+write.delim(STORE_DETAIL, "STORE_DETAIL.txt", sep = "|" )
 
 
 
--- Sample P2P Data.R
+++ Sample P2P Data.R
@@ -17,7 +17,8 @@ v_onetimeflag = c("Yes", "No"); v_onetimeflag_probs = c(0.015,1)
 v_nopaymentflag = c("Yes", "No"); v_nopaymentflag_probs = c(0.015,1)
 v_isemployee = c("Yes", "No"); v_isemployee_probs = c(0.015,0.5)
 v_cat = c("Medical","Utilities","Health & Safety","Offices Stationery Services","Taxi Services","Data Services","Employees","Associations","Advertising","Legal Services","Subcontractor","Consultancy","IT Service/Supplies","Insurance","Accountancy/Financial Services","Business Travel","Government Department","Marketing Services");v_cat_probs = runif(length(v_cat))
-v_paydays = c("0 Days","7 Days","14 Days","21 Days","30 Days","60 Days");v_paydays_probs = c(0.05, 0.15, 0.25, 0.5, 0.4,0.25)
+v_paydays = c(0,7,14,21,30,60);v_paydays_probs = c(0.05, 0.15, 0.25, 0.5, 0.4,0.25)
+terms = "Days"
 v_ctry = c("GBP", "EUR", "USD"); v_ctry_probs = c(1,0.1,0.05)
 v_tx_ctry = c("GBP", "EUR", "USD"); v_tx_ctry_probs = c(1,0.01,0.025)
 
@@ -38,16 +39,16 @@ ONE_TIME_FLAG = sample(v_onetimeflag, n_vendors,v_onetimeflag_probs, replace = T
 NO_PAYMENT_FLAG = sample(v_nopaymentflag, n_vendors,v_nopaymentflag_probs, replace = T)
 IS_EMPLOYEE = sample(v_isemployee, n_vendors,v_isemployee_probs, replace = T)
 
-VENDORS = data.frame(VENDOR_ID = VENDOR_ID,VENDOR_DESC = VENDOR_DESC,STATUS = STATUS, ADDRESS = ADDRESS,POST_CODE =POST_CODE,VENDOR_CAT = VENDOR_CAT,PAYMENT_TERMS = PAYMENT_TERMS,CREATE_DATE = CREATE_DATE,COUNTRY = COUNTRY,TAX_COUNTRY = TAX_COUNTRY,VEN_CREATE_ID = VEN_CREATE_ID,IBAN = IBAN,ONE_TIME_FLAG = ONE_TIME_FLAG,NO_PAYMENT_FLAG = NO_PAYMENT_FLAG,IS_EMPLOYEE = IS_EMPLOYEE)
+VENDORS = data.frame(VENDOR_ID = VENDOR_ID,VENDOR_DESC = VENDOR_DESC,STATUS = STATUS, ADDRESS = ADDRESS,POST_CODE =POST_CODE,VENDOR_CAT = VENDOR_CAT,PAYMENT_TERMS = PAYMENT_TERMS,TERMS = terms, CREATE_DATE = CREATE_DATE,COUNTRY = COUNTRY,TAX_COUNTRY = TAX_COUNTRY,VEN_CREATE_ID = VEN_CREATE_ID,IBAN = IBAN,ONE_TIME_FLAG = ONE_TIME_FLAG,NO_PAYMENT_FLAG = NO_PAYMENT_FLAG,IS_EMPLOYEE = IS_EMPLOYEE)
 
-write.delim(Vendors, "Vendors.txt", sep = "|")
+write.delim(VENDORS, "Vendors.txt", sep = "|")
 
 ##########     4) POs          ###############
 
 n_pos = 25700;n_names = 23; n_process_fte = 12
 po_s_date = '2020/01/01';po_e_date = '2021/12/31'
 po_ord_date = seq(as.Date(po_s_date), as.Date(po_e_date), by="day");
-po_ord_date_probs = seq(1,length(po_create_date),0.005)[1:length(po_create_date)]
+po_ord_date_probs = seq(1,length(po_ord_date),0.005)[1:length(po_ord_date)]
 sts = c("Finished","Sent","Rejected","Canceled","In Preparation","Acknowledgement Received","In Revision","In Approval");sts_probs = c(1,0.1,0.1,0.1,0.01,0.01,0.01,0.01)
 order_status = c("Ordered","Not Ordered");order_status_probs = c(1, 0.01)
 delivery_status = c("Delivered","Partially Delivered","Not Delivered");delivery_status_probs = c(1, 0.0025, 0.01)
@@ -82,10 +83,10 @@ write.delim(POs, "POs.txt", sep = "|")
 
 ##########     5) Invoices          ###############
 
-n_invs = 1017;n_products = 1:10;n_products_probs = runif(length(n_products))
+n_invs = 30421;n_products = 1:10;n_products_probs = runif(length(n_products))
 tax_rate = 0.2
 inv_s_date = '2020/01/01';inv_e_date = '2021/12/31'
-inv_date = seq(as.Date(inv_s_date), as.Date(inv_e_date), by="day");inv_date_probs = seq(1,length(po_create_date),0.005)[1:length(po_create_date)]
+inv_date = seq(as.Date(inv_s_date), as.Date(inv_e_date), by="day");inv_date_probs = seq(1,length(inv_date),0.005)[1:length(inv_date)]
 inv_create_date = -2:6;inv_create_date_probs = c(c(0.015,0.025),rep(1,length(inv_create_date)-2))
 
 inv_status = c("Paid","Partially Paid","Not Paid","Voided","Canceled");inv_status_probs = c(1, 0.15, 0.01, 0.01, 0.05)
@@ -94,13 +95,15 @@ inv_doc_type = c("Invoice","Credit Memo");inv_doc_type_probs = c(1, 0.005)
 inv_descrip = c("Fuel","Expenses","Printer expenses","Card Fees","Freight Charge-Courier Services","New office chair","Card","Mileage","Trip overseas","Hotel","Flights","Breakfast");inv_descrip_probs = runif(length(inv_descrip))
 inv_line_descrip = c("Fuel","Expenses","Printer expenses","Card Fees","Freight Charge-Courier Services","New office chair","Card","Mileage","Trip overseas","Hotel","Flights","Breakfast");inv_line_descrip_probs = runif(length(inv_line_descrip))
 inv_comp_codes = c("1000","1005","1010","1020"); inv_comp_codes_probs = c(1,0.8,0.25, 0.4)
-inv_comp_codes = c("200000","210000","220000","230000","240000","250000"); inv_comp_codes_probs = c(1,0.8,0.9, 0.1,0.25, 0.15)
+inv_cc_codes = c("200000","210000","220000","230000","240000","250000"); inv_cc_codes_probs = c(1,0.8,0.9, 0.1,0.25, 0.15)
 inv_gl_code = c("50000","50050","50100","50125","50150","50300","50400","50500","51000","52000","60100","60150","60200","60225","60250");inv_gl_code_probs = runif(length(inv_gl_code))
 inv_vendors = VENDOR_ID; inv_vendors_probs = runif(length(inv_vendors))
 inv_pay_terms = c(0,7,14,21,28,35,60,90); inv_pay_terms_probs = seq(10,length(inv_pay_terms),-(10-length(inv_pay_terms))/length(inv_pay_terms))[1:length(inv_pay_terms)]
 inv_po = c(1,0);inv_po_probs = c(0.6,0.4)
 inv_qty = 1:10; inv_qty_probs = seq(max(inv_qty),min(inv_qty),-1 )
 inv_gross_amt = c(seq(5,15000, 50), seq(50,25000, 150),seq(500,50000, 500));inv_gross_amt_probs =runif(inv_gross_amt)
+
+
 
 
 INV_ID = seq(100,((n_invs*1)+100),1)[1:n_invs]
@@ -125,14 +128,64 @@ INV_PO_ID_T = sample(inv_po, n_invs,inv_po_probs, replace = T);INV_PO_ID = ifels
 QTY = sample(inv_qty, n_invs,inv_qty_probs, replace = T)
 GROSS_AMT = sample(inv_gross_amt, n_invs,inv_gross_amt_probs, replace = T)
 TAX_AMT = GROSS_AMT * tax_rate 
-NET_AMT = GROSS_AMT *TAX_AMT
+NET_AMT = GROSS_AMT *(1 - tax_rate)
 UNIT_PRICE = round(GROSS_AMT/QTY ,2)
 UNIT = rep("Amount",n_invs)
+n_payments = length(INV_STS[INV_STS == "Paid"])
+pay_days = c(seq(1,21,3),seq(1,21,1),seq(1,14,1),seq(1,7,1));pay_days_probs = runif(pay_days)
+PO_DT= POs %>% select(INV_PO_ID = PO_ID,ORD_DT) %>% mutate(INV_PO_ID = as.character(INV_PO_ID))
+PO_DAYS_DIFF = c(seq(1,21,1),seq(1,15,3),seq(1,20,4),seq(1,7,1),seq(1,7,1));PO_DAYS_DIFF_PROBS = runif(length(PO_DAYS_DIFF))
 
-INVOICES = data.frame(INV_ID =INV_ID,INV_STS =INV_STS,DOC_STS =DOC_STS,DOC_TYPE =DOC_TYPE,INV_DT =INV_DT,INV_CREATE_DT =INV_CREATE_DT,INV_DESC =INV_DESC,INV_LINE_DESC =INV_LINE_DESC,
+
+INVOICES_T = data.frame(INV_ID =INV_ID,INV_STS =INV_STS,DOC_STS =DOC_STS,DOC_TYPE =DOC_TYPE,INV_DT =INV_DT,INV_CREATE_DT =INV_CREATE_DT,INV_DESC =INV_DESC,INV_LINE_DESC =INV_LINE_DESC,
                       INV_LINE_ID =INV_LINE_ID,INV_EXT_DOC_ID =INV_EXT_DOC_ID,PRODUCT =PRODUCT,COMP_CODE =COMP_CODE,COST_CNTR =COST_CNTR,GL_CODE =GL_CODE,PAYMENT_TERMS =PAYMENT_TERMS,
-                      DUE_DATE =DUE_DATE,PAYMENT_TYPE =PAYMENT_TYPE,INV_SUPPLIER_ID =INV_SUPPLIER_ID,INV_PO_ID =INV_PO_ID,QTY =QTY,GROSS_AMT =GROSS_AMT,TAX_AMT = TAX_AMT,NET_AMT =NET_AMT,UNIT_PRICE =UNIT_PRICE,UNIT =UNIT)
+                      DUE_DATE =DUE_DATE,PAYMENT_TYPE =PAYMENT_TYPE,INV_SUPPLIER_ID =INV_SUPPLIER_ID,INV_PO_ID =INV_PO_ID,QTY =QTY,GROSS_AMT =GROSS_AMT,TAX_AMT = TAX_AMT,NET_AMT =NET_AMT,UNIT_PRICE =UNIT_PRICE,UNIT =UNIT) %>% 
+                      mutate(days_pay = ifelse(INV_STS == "Paid", sample(pay_days, n_payments,pay_days_probs, replace = T), 0)) %>% left_join(PO_DT) %>% 
+                      mutate(INV_DT_ADD = ifelse(is.na(ORD_DT), INV_DT , as.Date(ORD_DT + sample(PO_DAYS_DIFF,n(), PO_DAYS_DIFF_PROBS, replace = T)))) %>% 
+                      mutate(INV_DT = as.Date(INV_DT_ADD, origin = "1970-01-01"),PAY_DATE = INV_DT + days_pay) %>% select(-INV_DT_ADD, -ORD_DT,-days_pay) 
 
+head(INVOICES_T)
+
+##########     8) Create tests for exceptions ###############
+options(digits=15)
+INV_SUMM = INVOICES_T %>%  group_by(INV_ID) %>% 
+  summarise(GROSS_AMT = round(sum(GROSS_AMT),2), N_INV = n()) %>% data.frame()
+
+##########     9) Test Layer ###############
+
+#### I_01_DUPE - duplicate invoices ####
+I_01_DUPE = INV_SUMM %>% filter(N_INV > 1) %>% select(INV_ID, I_01_DUPE = N_INV)
+
+
+#### I_02_BLKDVEN - Blocked Vendors ####
+INV_VENDOR_SUMM = INVOICES_T %>%  group_by(INV_ID,INV_SUPPLIER_ID ) %>% 
+  summarise(GROSS_AMT = round(sum(GROSS_AMT),2), N_INV = n()) %>% data.frame()
+VEN_STS = VENDORS %>% select(INV_SUPPLIER_ID  = VENDOR_ID,STATUS)
+
+I_02_BLKDVEN = left_join(INV_VENDOR_SUMM,VEN_STS) %>% filter(STATUS == "Blocked") %>% 
+  select(INV_ID, STATUS )
+
+
+#### I_03_PAYDATE_INVDATE - Blocked Vendors ####
+I_03_PAYDATE_INVDATE = INVOICES_T %>% filter(INV_CREATE_DT > INV_DT) %>% select(INV_ID, INV_GA =GROSS_AMT ) 
+
+#### I_04_LRG_INV_WO_PO - Blocked Vendors ####
+I_04_LRG_INV_WO_PO = INVOICES_T %>% filter(INV_PO_ID == " " & GROSS_AMT > 15000) %>% select(INV_ID, INV_GA =GROSS_AMT )
+
+
+#### Consolidate tests ####
+
+INV_TEST_RESULTS =  left_join(INV_SUMM, I_01_DUPE, by = "INV_ID") %>% mutate(I_01_DUPE = ifelse(is.na(I_01_DUPE) ,0,1))  %>% 
+  left_join(I_02_BLKDVEN) %>% mutate(I_02_BLKDVEN = ifelse(is.na(STATUS) ,0,1)) %>% select(-STATUS) %>% 
+  left_join(I_03_PAYDATE_INVDATE) %>% mutate(I_03_PAYDATE_INVDATE = ifelse(is.na(INV_GA) ,0,1)) %>% select(-INV_GA) %>%
+  left_join(I_04_LRG_INV_WO_PO) %>% mutate(I_04_LRG_INV_WO_PO = ifelse(is.na(INV_GA) ,0,1)) %>% select(-INV_GA) %>%
+  filter(I_01_DUPE != 0 | I_02_BLKDVEN != 0 | I_03_PAYDATE_INVDATE != 0 | I_04_LRG_INV_WO_PO != 0) %>% 
+  select(INV_ID, I_01_DUPE, I_02_BLKDVEN, I_03_PAYDATE_INVDATE, I_04_LRG_INV_WO_PO)
+
+INVOICES =  left_join(INVOICES_T,INV_TEST_RESULTS) %>% 
+    mutate(I_01_DUPE = ifelse(is.na(I_01_DUPE),0,I_01_DUPE),I_02_BLKDVEN = ifelse(is.na(I_02_BLKDVEN),0,I_02_BLKDVEN),
+           I_03_PAYDATE_INVDATE = ifelse(is.na(I_03_PAYDATE_INVDATE),0,I_03_PAYDATE_INVDATE),I_04_LRG_INV_WO_PO = ifelse(is.na(I_04_LRG_INV_WO_PO),0,I_04_LRG_INV_WO_PO)
+      ) 
 
 write.delim(INVOICES, "Invoices.txt", sep = "|")
 
@@ -141,6 +194,20 @@ write.delim(INVOICES, "Invoices.txt", sep = "|")
 
 
 
+
+
+
+## Remove vendor tables ##
+rm(n_vendors);rm(n_users); rm(n_users_probs); rm(ven_s_date);rm(ven_e_date);rm(v_create_date);rm(v_create_date_probs);rm(v_status); rm(v_status_probs);rm(v_onetimeflag); rm(v_onetimeflag_probs);
+rm(v_nopaymentflag); rm(v_nopaymentflag_probs);rm(v_isemployee); rm(v_isemployee_probs);rm(v_cat);rm(v_cat_probs);rm(v_paydays);rm(v_paydays_probs);rm(v_ctry); rm(v_ctry_probs);rm(v_tx_ctry); rm(v_tx_ctry_probs)
+
+## Remove PO tables ##
+rm(n_pos);rm(n_names);rm(n_process_fte);rm(po_s_date);rm(po_e_date);rm(po_ord_date);rm(po_ord_date_probs);rm(sts);rm(sts_probs);rm(order_status);rm(order_status_probs);rm(delivery_status);rm(delivery_status_probs);
+rm(invoice_status);rm(invoice_status_probs);rm(po_vendors); rm(po_vendors_probs);rm(user_names);rm(user_names_probs);rm(create_names);rm(create_names_probs);rm(n_items); rm(n_items_probs);rm(po_amts);rm(po_amts_probs)
+
+## Remove invoice tables ##
+rm(INV_ID);rm(INV_STS);rm(DOC_STS);rm(DOC_TYPE);rm(INV_DT);rm(INV_CREATE_DT);rm(INV_DESC);rm(INV_LINE_DESC);rm(INV_LINE_ID);rm(INV_EXT_DOC_ID);rm(PRODUCT);rm(COMP_CODE);rm(INVOICES_T);
+rm(COST_CNTR);rm(GL_CODE);rm(PAYMENT_TERMS);rm(DUE_DATE);rm(PAYMENT_TYPE);rm(INV_SUPPLIER_ID);rm(INV_PO_ID_T);rm(QTY);rm(GROSS_AMT);rm(TAX_AMT);rm(NET_AMT);rm(UNIT_PRICE);rm(UNIT);rm(n_payments);rm(pay_days);rm(I_01_DUPE);rm(I_02_BLKDVEN);rm(I_03_PAYDATE_INVDATE);rm(I_04_LRG_INV_WO_PO)
 
 
 
